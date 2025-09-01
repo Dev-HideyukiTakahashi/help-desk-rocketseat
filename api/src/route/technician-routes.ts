818cc8af3ff0d664288172e5e5ed52c7456d564f
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-09-01 01:20:58 -0300

    authorization with roles

--- api/src/route/technician-routes.ts
+++ api/src/route/technician-routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { TechnicianController } from '../controller/technician-controller';
 import { ensureAuthenticated } from '../middleware/ensure-authenticated';
+import { verifyAuthorization } from '../middleware/verify-authorization';
 
 const technicianRoutes = Router();
 const technicianController = new TechnicianController();
 
 // middlewares
 technicianRoutes.use(ensureAuthenticated);
+technicianRoutes.use(verifyAuthorization(['ADMIN']));
 
 // routes
 technicianRoutes.post('/', technicianController.create);
